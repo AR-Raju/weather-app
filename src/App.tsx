0add8bbb07d5c12517a2a7092a978911bdd09855
AR-Raju <asikurrahmanraju@gmail.com>
2022-02-04 12:48:53 +0600

    router test case added

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import CountryInfo from "./components/CountryInfo";
-import PageNotFound from "./components/PageNotFound";
 
 const NoMatch = () => <div>No match</div>;
 
